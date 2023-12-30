d2aba1e7b9114130aa0086acccc45f45cdfa1114
xoxohorses <69382434+xoxohorses@users.noreply.github.com>
2023-12-30 13:20:28 -0800

    init index.ts

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -1,9 +1,10 @@
-const express = require("express");
+import express from "express";
+
 const app = express();
-const port = 3001; // use a different port from your React app
+const port = 3001;
 
 app.get("/", (req, res) => {
-  res.send("Hello from Express!");
+  res.send("Express server working!");
 });
 
 app.listen(port, () => {
