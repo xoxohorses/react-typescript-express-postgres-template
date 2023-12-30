5be3d4b1149603cceed5839c216bd58c50a8f67a
xoxohorses <69382434+xoxohorses@users.noreply.github.com>
2023-12-30 13:38:08 -0800

    use cors

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -1,8 +1,15 @@
+import cors from "cors";
 import express from "express";
 
 const app = express();
 const port = 3001;
 
+const corsOptions = {
+  origin: "http://localhost:3000",
+  methods: ["GET", "PATCH", "POST"],
+};
+
+app.use(cors(corsOptions));
 app.use(express.json({ type: "application/json" }));
 
 app.get("/", (req, res) => {
