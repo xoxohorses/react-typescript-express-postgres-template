8f71c8dbe5b7d8d550ea7039ad509d2362501e10
xoxohorses <69382434+xoxohorses@users.noreply.github.com>
2023-12-31 18:56:22 -0800

    setup /users

--- packages/server/db/index.ts
+++ packages/server/db/index.ts
@@ -1,7 +1,25 @@
-import { Pool } from "pg";
+import pg from "pg";
 
-const pool = new Pool();
+// Env variables configued in docker-compose.yml
+const pool = new pg.Pool({
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  port: 5432,
+});
 
-export async function query(query: { text: string; values: Array<unknown> }) {
-  return await pool.query(query);
+export async function query(query: { text: string; values?: Array<unknown> }) {
+  try {
+    const response = await pool.query(query);
+    return {
+      status: "success",
+      data: response,
+    };
+  } catch (error) {
+    return {
+      status: "error",
+      error,
+    };
+  }
 }
