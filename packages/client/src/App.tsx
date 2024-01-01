cb2c53f86070e6f5fc3d38a5e6e332b1c66f3761
Julie <juliemyu@gmail.com>
2023-12-31 23:31:20 -0800

    [TASK-19] Readme (#8)

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-import { sharedFunction } from "../../shared/src/utils";
+import { sharedFunction } from "../../shared/utils";
 
 const App = () => {
   sharedFunction();
