1c11af428461ab701202ce27147cc73fca095d2c
xoxohorses <69382434+xoxohorses@users.noreply.github.com>
2024-01-01 00:28:53 -0800

    import css file

--- packages/client/src/index.tsx
+++ packages/client/src/index.tsx
@@ -1,6 +1,8 @@
+import "./styles.css";
+
 import React from "react";
 import ReactDOM from "react-dom";
 
-import App from "./src/App";
+import App from "./App";
 
 ReactDOM.render(<App />, document.getElementById("root"));
