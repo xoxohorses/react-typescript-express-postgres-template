ed3766788cae8cd51b892649b504f146d480772c
xoxohorses <69382434+xoxohorses@users.noreply.github.com>
2024-01-07 15:53:12 -0800

    tailwindcss

--- packages/client/webpack.config.cjs
+++ packages/client/webpack.config.cjs
@@ -22,7 +22,13 @@ module.exports = {
       },
       {
         test: /\.css$/,
-        use: ["style-loader", "css-loader"],
+        use: [
+          { loader: "style-loader" },
+          { loader: "css-loader", options: { importLoaders: 1 } },
+          { loader: "postcss-loader" },
+        ],
+        include: path.resolve(__dirname, "src"),
+        exclude: /node_modules/,
       },
     ],
   },
